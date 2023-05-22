94bd26bcb342f69dbe69cc29c4550aa472d4789c
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-05-22 19:21:33 +0200

    warmupIterations, cliProgressBar, compareResults, printSpecs

--- app/src/highlvlBenchmark.ts
+++ app/src/highlvlBenchmark.ts
@@ -1,5 +1,10 @@
 import timoi from "timoi"
 import delay from "delay"
+import os from "os"
+import osName from "os-name"
+import getSpecs from "cross-platform-specs"
+import { deepEqual } from "fast-equals"
+import { SingleBar, MultiBar, Presets } from "cli-progress"
 
 
 
@@ -7,23 +12,69 @@ const orderedSpeedAdjectiveFac = [2, 1.35, 1]
 const speedAdjectives = {
   1: "slightly",
   1.35: "considerably",
-  2: "hugely"
+  2: "much"
 }
 
-export function benchmarkSuite(iterations = 1000000) {
-  // Todo print cpu and mem information
-  
+
+export function benchmarkSuite(iterations = 1000000, { warmupIterations, cliProgressBar, compareResults, printSpecs } = { warmupIterations: Math.ceil(iterations / 1000), cliProgressBar: true, compareResults: true, printSpecs: true }) {
+  if (printSpecs) console.log(getSpecs())
+
+
 
   return async function benchmark(...fs: (() => ((i: number) => void))[]) {
-    console.log("Starting benchmark")
 
-    const timings = []
+    let multiProgressBar: MultiBar
+
+    if (cliProgressBar) {
+      multiProgressBar = new MultiBar({
+        clearOnComplete: false,
+        hideCursor: true,
+        format: "{name} | {bar} | {percentage}% | {eta_formatted}"
+      }, Presets.legacy)
+    }
+
+    if (warmupIterations) {
+      let bar: SingleBar
+      if (cliProgressBar) bar = multiProgressBar.create(fs.length * warmupIterations, 0, { name: "Warmup" })
+      else console.log("Warming up")
+
+      for (let i = 0; i < fs.length; i++) {
+        await delay(100)
+        const call = fs[i]()
+        const oldValue = call(0)
+        if (cliProgressBar) bar.increment()
+        for (let j = 1; j < warmupIterations; j++) {
+          const newValue = call(j)
+          if (compareResults && !deepEqual(oldValue, newValue)) throw new Error("Results must be always the equal.")
+          if (cliProgressBar) bar.increment()
+        }
+      }
+
+    }
+    
+
+
+    
+    let bar: SingleBar
+    if (cliProgressBar) bar = multiProgressBar.create(fs.length * warmupIterations, 0, { name: "Benchmark" })
+    else console.log("Benchmarking")
+
+
+    
+    
+
+
+    const timings = [] as number[]
     for (let i = 0; i < fs.length; i++) {
       await delay(100)
       const time = timoi()
       const call = fs[i]()
-      for (let i = 0; i < iterations; i++) {
-        call(i)
+      const oldValue = call(0)
+      if (cliProgressBar) bar.increment()
+      for (let j = 1; j < iterations; j++) {
+        call(j)
+        if (compareResults && !deepEqual(oldValue, call(j))) throw new Error("Results must be always the equal.")
+        if (cliProgressBar) bar.increment()
       }
       timings.push(time.time())
       console.log(`${fs[i].name ? fs[i].name : ((i + 1) + ". run")} took ${time.str()}`)
@@ -38,7 +89,7 @@ export function benchmarkSuite(iterations = 1000000) {
         let percent = Math.round(fac * 100)
         let fastDesc: string
         if (percent < 100) {
-          let desc: string
+          let desc: string = ""
           for (const speed of orderedSpeedAdjectiveFac) {
             if (percent < 100 / (+speed)) {
               desc = speedAdjectives[speed]
@@ -48,7 +99,7 @@ export function benchmarkSuite(iterations = 1000000) {
           fastDesc = desc + " faster"
         }
         else if (percent > 100) {
-          let desc: string
+          let desc: string = ""
           for (const speed of orderedSpeedAdjectiveFac) {
             if (percent > 100 * (+speed)) {
               desc = speedAdjectives[speed]

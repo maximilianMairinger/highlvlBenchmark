626b8246cf30d9fc25fa1072e03e2866261a5848
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-05-26 00:23:08 +0200

    fix CI

--- CI/deno.mjs
+++ CI/deno.mjs
@@ -1,6 +1,45 @@
-import * as spec from "./../app/dist/esm/highlvlBenchmark.mjs"
-import all from "./../app/dist/esm/highlvlBenchmark.mjs"
+import benchmarkSuite from "./../app/dist/esm/highlvlBenchmark.mjs"
 
-console.log(all())
-console.log()
-console.log(spec)
+import fastList from "fast-list"
+import yallist from "yallist"
+import fastLinkedList from "fast-linked-list"
+
+const itr = 10000000
+const benchmark = benchmarkSuite(itr)
+
+
+benchmark(
+  function Yallist() {
+    const list = yallist.create()
+
+    return (j) => {
+      if (j % 2) list.push(j)
+      else list.unshift(j)
+    }
+  },
+  function FastLinkedList() {
+    const list = new fastLinkedList()
+
+    return (j) => {
+      if (j % 2) list.push(j)
+      else list.unshift(j)
+    }
+  },
+  function FastList() {
+
+    const list = new fastList()
+
+    return (j) => {
+      if (j % 2) list.push(j)
+      else list.unshift(j)
+    }
+  },
+  function Array() {
+    const list = []
+    
+    return (j) => {
+      if (j % 2) list.push(j)
+      else list.unshift(j)
+    }
+  }
+)
